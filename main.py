f21f1f0bda7f3d469257828a259d80e939677383
DanteNiewenhuis <d.niewenhuis@hotmail.com>
2022-01-18 13:59:53 +0100

    update

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import jgrapht
 
-filename = "./data/logging-log4j2-rel-2.12.1-rapid/rapid/org.apache.logging.log4j.log4j-iostreams-2.12.1-merged.json"
-
-filename = "./data/logging-log4j2-rel-2.12.1-rapid/rapid/org.apache.logging.log4j.log4j-cassandra-2.12.1-merged.json"
+filename = "./data/logging-log4j2-rel-2.12.1-rapid/rapid/org.apache.logging.log4j.log4j-docker-2.12.1-reduced.json"
+# filename = "./data/logging-log4j2-rel-2.12.1-rapid/rapid/org.apache.logging.log4j.log4j-cassandra-2.12.1-merged.json"
 v_attrs = {}
 
 
@@ -16,7 +15,36 @@ def vertex_attribute_cb(vertex, attribute_name, attribute_value):
         Adds the attributes to v_attrs dict, with node id as key."""
     if vertex not in v_attrs:
         v_attrs[vertex] = {}
-    v_attrs[vertex][attribute_name] = attribute_value
+
+    if attribute_name == 'application_node':
+        v_attrs[vertex][attribute_name] = (attribute_value == 'true')
+    else:
+        v_attrs[vertex][attribute_name] = attribute_value
+
+
+def list_external_dependencies_application(g, v_attrs):
+    """List the edges from an application_node to a non application_node"""
+    ext_depend = []
+    for v in g.vertices:
+        if v_attrs[v]['application_node']:
+            for e in g.outedges_of(v):
+                if not v_attrs[g.edge_target(e)]['application_node']:
+                    ext_depend.append(g.edge_target(e))
+
+    return ext_depend
+
+
+def list_external_dependencies(g, v_attrs):
+    g_dep = jgrapht.create_graph(directed=True, weighted=False,
+                                 allowing_self_loops=True, allowing_multiple_edges=False, any_hashable=True)
+    for e in g.edges:
+        if not (v_attrs[g.edge_target(e)]['product'] == v_attrs[g.edge_source(e)]['product']):
+            g_dep.add_vertex(v_attrs[g.edge_source(e)]['product'])
+            g_dep.add_vertex(v_attrs[g.edge_target(e)]['product'])
+            g.add_edge(v_attrs[g.edge_source(e)]['product'],
+                       v_attrs[g.edge_target(e)]['product'])
+
+    return g_dep
 
 
 def main():
@@ -26,14 +54,13 @@ def main():
                                    import_id_cb=import_id_cb,
                                    vertex_attribute_cb=vertex_attribute_cb)
 
-    for v in g.vertices:
-        print('Vertex {}'.format(v))
-        edges = g.outedges_of(v)
+    # ext_depend = list_external_dependencies_application(g, v_attrs)
+    # print(len(ext_depend))
+    # print(ext_depend)
+    # print([v_attrs[v]['product'] for v in ext_depend])
+    # print(len(set([v_attrs[v]['product'] for v in ext_depend])))
 
-        print(v_attrs[v]['ID'])
-
-        for e in edges:
-            print('Vertex {} edge {}'.format(v, e))
+    ext_depend_graph = list_external_dependencies(g, v_attrs)
 
 
 if __name__ == "__main__":
